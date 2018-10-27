4066b7da608d5bcd11f60511776ae5a7cdd27592
Allen Hai <allenhai03@gmail.com>
2018-10-27 01:00:13 -0500

    generate random secret and run infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use std::io;
-use std::io::Read;
-use std::fs::File;
+//use std::io::Read;
+// use std::fs::File;
 
-fn main() -> io::Result<()> {
-    println!("- guessing game -");
+extern crate rand;
 
-    let mut x = File::open("./x")?;
-    let mut buffer = [0; 16];
-    let mut guess = String::new();
+use rand::Rng;
 
-    x.read(&mut buffer)?;
-    
-    io::stdin().read_line(&mut guess)
-        .expect(":/ couldn't read line");
-    
-    // removes trailing new-line char
-    guess.pop();
-    
-    println!("[{}]", guess);
-    println!("{:?}", buffer);
+fn main() {
+    loop {
+        println!("- guessing game -");
 
-    Ok(())
+     // let mut x = File::open("./x")?;
+     // let mut buffer = [0; 16];
+        let mut guess = String::new();
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        
+
+     // x.read(&mut buffer)?;
+        
+        io::stdin().read_line(&mut guess)
+            .expect(":/ couldn't read line");
+
+     // removes trailing new-line char
+        guess.pop();
+        
+        println!("[{}:{}]", guess, secret_number);
+     // println!("{:?}", buffer);
+    }
 }
